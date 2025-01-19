0b6329c1d78d5a4668aefe1e64df4031f623eaf6
subhajyoti.cumulus <cumulus.aws22@gmail.com>
2025-01-19 14:19:52 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "networking" {
-  source               = "./networking"
+  source               = "./modules/vpc_module"
   vpc_cidr             = var.vpc_cidr
   vpc_name             = var.vpc_name
   cidr_public_subnet   = var.cidr_public_subnet
